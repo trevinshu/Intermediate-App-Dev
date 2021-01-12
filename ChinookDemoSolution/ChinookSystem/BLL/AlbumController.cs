9fcd92b94c681473f90ed34441371f2fb883414e
trevinshu <trevinshu2008@gmail.com>
2021-01-12 16:35:35 -0700

    create albums controller, view model class , artist albums

---
+++ ChinookDemoSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Entities;
+using ChinookSystem.DAL;
+using ChinookSystem.ViewModels;
+using System.ComponentModel; //For ODS
+#endregion
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        //Can't use entity class as a return type because entities are internal
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<ArtistAlbums> Albums_GetArtistAlbums()
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                //Linq to Entity
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }
+        }
+    }
+}
